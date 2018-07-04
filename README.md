226642bcc5fe630a21401d60a5102803b2a857f3
Mohamed Emad <m.emad550@gmail.com>
2018-07-05 00:25:06 +0200

    first launch for application

---
+++ README.md
@@ -0,0 +1,15 @@
+# imágenes
+
+imágenes is a sample application use multiple APIs you can use it to get Images from unsplash and some articles from New Yourk Times all you.
+
+you can see an image. ![image](./images/capture.png "Title")
+
+here is [live preview](https://muhammedemad.github.io/im-genes/)
+
+### how to use?
+just you need to write what you search about and press submit.
+
+### is this web application Responsive!?
+ofcourse.
+
+
---
+++ js/app.js
@@ -0,0 +1,61 @@
+(function () {
+    const form = document.querySelector('#search-form');
+    const searchField = document.querySelector('#search-keyword');
+    let searchedForText;
+    const responseContainer = document.querySelector('#response-container');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        responseContainer.innerHTML = '';
+        searchedForText = searchField.value;
+
+        // search for an Image in unsplash which using Request header
+        function addImage() {
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+            if (data && data.results) {
+                htmlContent = data.results.map(img => `<figure>
+                    <img src="${img.urls.regular}" alt="${searchedForText}">
+                    <figcaption>${searchedForText} by ${img.user.name}</figcaption>
+                </figure>`).join('');
+            } else {
+                htmlContent = '<div class="error-no-image">No images available</div>';
+            }
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
+        const unsplashRequest = new XMLHttpRequest();
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.onerror = function (err) {
+            console.log(err);
+        };
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID 7210b5c5405f766b2f0298ae731709557a8a7275688387c5162150342d228255');
+        unsplashRequest.send();
+
+        // search for an Article in NYT which not using Request header
+        function addArticles() {
+            let htmlContent = '';
+            // convert response from json to html object
+            const data = JSON.parse(this.responseText);
+            if (data && data.response && data.response.docs && data.response.docs.length > 1) {
+                // if more than on articule has been returned maps over each article and then returns a list item that contains the article's
+                htmlContent = '<ul>' + data.response.docs.map(article => `
+                    <li class='article'>
+                        <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                        <p>${article.snippet}</p>
+                    </li>`
+                ).join('') + '</ul>';
+            } else {
+                htmlContent = '<div class="error-no-articles">No articles available</div>';
+            }
+            responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+        }
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=2673bdbbdee9493ba01ac2e41ed8d941`);
+        articleRequest.send();
+        articleRequest.onload = addArticles;
+        articleRequest.onerror = function (err) {
+            console.log(err);
+        };
+    });
+})();
