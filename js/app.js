0eb9962210f8de9c0e02d17a95e2d3bff0d632df
Mohamed Emad <m.emad550@gmail.com>
2018-07-07 19:58:17 +0200

    Reactor: using fetch API instead of ajax

--- js/app.js
+++ js/app.js
@@ -1,63 +1,55 @@
-/* eslint-env jquery */
-
-const form = $('#search-form'),
-    searchField = $('#search-keyword'),
-    responseContainer = $('#response-container'),
-    imagesContainer = $('#images'),
-    articlesContainer = $('#articles');
+const form = document.querySelector('#search-form');
+const searchField = document.querySelector('#search-keyword');
 let searchedForText;
+const responseContainer = document.querySelector('#response-container');
+let imagesContainer = document.querySelector('#images'),
+    articlesContainer = document.querySelector('#articles');
 
-
-form.on('submit', function (e) {
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-    responseContainer.html('');
-    searchedForText = searchField.val();
-    unsplashRequest();
-    nytRequest();
+    searchedForText = searchField.value;
+    imagesRequest();
+    articleRequest();
 });
-
-function unsplashRequest() {
-    // set usplash API "image request"
-    $.ajax({
-        url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+// set usplash API "image request"
+function imagesRequest() {
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
         headers: {
             Authorization: 'Client-ID 7210b5c5405f766b2f0298ae731709557a8a7275688387c5162150342d228255'
         }
-    }).done(addImage).fail(function (err) {
-        console.log(err, 'image');
-    });
+    }).then(response => response.json())
+        .then(addImage)
+        .catch(err => requestError(err, 'image'));
 }
-// set NYT API "article request"
-function nytRequest() {
-    $.ajax({
-        url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=2673bdbbdee9493ba01ac2e41ed8d941`,
 
-    }).done(addArticles).fail(function (err) {
-        console.log(err, 'article');
-    });
-}
 function addImage(data) {
-    let htmlContent;
+    let htmlContent = '';
     if (data && data.results) {
-        const images = data.results;
-        htmlContent = images.map(image => `<figure>
-            <img src="${image.urls.regular}" alt="${searchedForText}">
-            <figcaption>${searchedForText} by ${image.user.name}</figcaption>
+        htmlContent = data.results.map(img => `<figure>
+            <img src="${img.urls.regular}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${img.user.name}</figcaption>
         </figure>`).join('');
     } else {
         htmlContent = '<div class="error-no-image">No images available</div>';
     }
-    imagesContainer.html(htmlContent);
+    imagesContainer.insertAdjacentHTML('afterbegin', htmlContent);
 }
 
+// set NYT API "article request"
+function articleRequest() {
+    fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=2673bdbbdee9493ba01ac2e41ed8d941`)
+        .then(response => response.json())
+        .then(addArticles)
+        .catch(err => requestError(err, 'article'));
+
+}
 // search for an Article in NYT which not using Request header
 function addArticles(data) {
-    let htmlContent;
+    let htmlContent = '';
     // convert response from json to html object
     if (data && data.response && data.response.docs && data.response.docs.length > 1) {
-        const articles = data.response.docs;
         // if more than on articule has been returned maps over each article and then returns a list item that contains the article's
-        htmlContent = '<ul>' + articles.map(article => `
+        htmlContent = '<ul>' + data.response.docs.map(article => `
             <li class='article'>
                 <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
                 <p>${article.snippet}</p>
@@ -65,5 +57,9 @@ function addArticles(data) {
     } else {
         htmlContent = '<div class="error-no-articles">No articles available</div>';
     }
-    articlesContainer.html(htmlContent);
+    articlesContainer.insertAdjacentHTML('beforeend', htmlContent);
+}
+function requestError(err, part) {
+    console.log(err);
+    responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
 }
